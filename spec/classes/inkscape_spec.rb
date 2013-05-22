d0daaf70946ee6f6fe4872f7c3eeb6a8a1a13abd
Larry Glenn <yrral.glenn@gmail.com>
2013-05-22 12:01:21 -0400

    Add fixtures and spec for include inkscape

--- spec/classes/inkscape_spec.rb
+++ spec/classes/inkscape_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe 'inkscape' do
   it do
+    should include_class('xquartz')
+  end
+  it do
     should contain_package('Inkscape').with( { :provider => 'appdmg', :source => 'http://downloads.sourceforge.net/inkscape/Inkscape-0.48.2-1-SNOWLEOPARD.dmg' } )
   end
 end
